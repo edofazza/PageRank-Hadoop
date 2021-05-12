2fd23b7b686f0d7f7adc13813d38927a2977ed62
edofazza <fazzari.edoardo@gmail.com>
2021-05-12 10:29:12 +0200

    Update CountNodesReducer.java

--- src/main/java/it/unipi/hadoop/pagerank/countnodes/CountNodesReducer.java
+++ src/main/java/it/unipi/hadoop/pagerank/countnodes/CountNodesReducer.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
-import java.util.function.Consumer;
 
 /**
  * This class implements the Reducer that is in charge of counting the number of nodes
