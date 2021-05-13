2338c4ce18352fe1437f1a75f9c5bacf23551713
edofazza <fazzari.edoardo@gmail.com>
2021-05-13 21:16:05 +0200

    Update SortingMapper.java

--- src/main/java/it/unipi/hadoop/pagerank/sortingMR/SortingMapper.java
+++ src/main/java/it/unipi/hadoop/pagerank/sortingMR/SortingMapper.java
@@ -5,16 +5,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
-public class SortingMapper extends Mapper<Text, Text, Text, Text> {
-    private Text outputKey = new Text();;
+public class SortingMapper extends Mapper<Object, Text, Text, Text> {
+    private Text outputKey = new Text();
+    private Text outputValue = new Text();
 
     @Override
-    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-        String pagerank = value.toString().trim().split(",")[0];
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] keyValueSplit = value.toString().split("\t");
+        outputValue.set(keyValueSplit[0]);
+        String pagerank = keyValueSplit[1].trim().split(",")[0];
         outputKey.set(pagerank);
 
         // pagerank as key in order to have automatic sorting
         // title of the page as value
-        context.write(outputKey, key);
+        context.write(outputKey, outputValue);
     }
 }
