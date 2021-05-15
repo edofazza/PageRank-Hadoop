15d7fe41fa0e3f4f8c1edf5632d38994763454d5
danielecioffo <d.cioffo@studenti.unipi.it>
2021-05-15 19:08:00 +0200

    Descending order in the output file

--- src/main/java/it/unipi/hadoop/pagerank/sortingMR/DescendingRankComparator.java
+++ src/main/java/it/unipi/hadoop/pagerank/sortingMR/DescendingRankComparator.java
@@ -4,6 +4,9 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
+/**
+ * Comparator used for ordering in descending order the intermediate keys in the Sorting Phase
+ */
 public class DescendingRankComparator extends WritableComparator {
     protected DescendingRankComparator() {
         super(DoubleWritable.class, true);
@@ -13,7 +16,7 @@ public class DescendingRankComparator extends WritableComparator {
     public int compare(WritableComparable w1, WritableComparable w2) {
         DoubleWritable key1 = (DoubleWritable) w1;
         DoubleWritable key2 = (DoubleWritable) w2;
-        return -1 * key1.compareTo(key2);
+        return -1 * key1.compareTo(key2); // the opposite of the default behavior
     }
 }
 
